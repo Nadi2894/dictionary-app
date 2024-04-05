203c7378a20cf2b8a81695600b9fcab0df1ad1c4
Nadi2894 <nadirabegimnadyrbekova@gmail.com>
2024-04-05 14:49:14 +0200

    axios added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyWord, setKeyword] = useState("");
 
+  function handleResponse(response) {}
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyWord}`);
+
+    let apiUrl =
+      "https://api.shecodes.io/dictionary/v1/define?word={word}&key=a41b6cct8f269ddeo4da44bf9300d179";
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
